629d015c1c95346cfb622fb511af4bd3e406e8d1
Артем Шестаков <artem.s.shestakov@yandex.ru>
2021-02-24 20:52:05 +0300

    first commit

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/artem-shestakov/go/handlers"
 )
@@ -18,9 +20,29 @@ func main() {
 	sm.Handle("/", hh)
 	sm.Handle("/bye", gh)
 
-	err := http.ListenAndServe(":8080", sm)
-	if err != nil {
-		fmt.Println(err)
+	s := http.Server{
+		Addr:         ":8080",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
 	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 
 }
