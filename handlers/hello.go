faeb8c0df865f5fc2c75a9ba64790be1e6b26c02
Артем Шестаков <artem.s.shestakov@yandex.ru>
2021-02-24 17:58:57 +0300

    first commit

---
+++ handlers/hello.go
@@ -0,0 +1,27 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServerHTTP(w http.ResponseWriter, r *http.Request) {
+	h.l.Println("Hello world")
+	d, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Oops", http.StatusBadRequest)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello %s\n", d)
+}
