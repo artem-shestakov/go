b4dab2b5305d5a969d3e76238b29687eb01e4f6f
Артем Шестаков <artem.s.shestakov@yandex.ru>
2021-02-24 20:06:46 +0300

    first commit

--- main.go
+++ main.go
@@ -12,9 +12,11 @@ import (
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
 
 	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handler("/bye", gh)
 
 	err := http.ListenAndServe(":8080", sm)
 	if err != nil {
