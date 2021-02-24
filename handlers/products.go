950e7f7534122df8041fb005a334bfddcfcefc4d
Артем Шестаков <artem.s.shestakov@yandex.ru>
2021-02-24 21:56:34 +0300

    .

---
+++ handlers/products.go
@@ -0,0 +1,20 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/artem-shestakov/go/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	lp := data.GetProducts()
+}
